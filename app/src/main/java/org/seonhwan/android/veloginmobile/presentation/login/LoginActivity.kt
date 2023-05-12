db4635ff63a84f83efa977e7f44af6c0eff52dc3
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-05-12 23:18:30 +0900

    feat : 비밀번호 숨김, 보임처리 #1

---
+++ app/src/main/java/org/seonhwan/android/veloginmobile/presentation/login/LoginActivity.kt
@@ -0,0 +1,48 @@
+package org.seonhwan.android.veloginmobile.presentation.login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import org.seonhwan.android.veloginmobile.MainActivity
+import org.seonhwan.android.veloginmobile.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        this.onClickLoginButton()
+        this.onClickPasswordVisibleButton()
+    }
+
+    private fun onClickLoginButton() {
+        with(binding) {
+            btLoginLogin.setOnClickListener {
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(intent)
+                if (!isFinishing) finish()
+            }
+        }
+    }
+
+    private fun onClickPasswordVisibleButton() {
+        with(binding) {
+            ivLoginPasswordVisible.setOnClickListener {
+                when (etLoginPassword.transformationMethod) {
+                    HideReturnsTransformationMethod.getInstance() -> {
+                        etLoginPassword.transformationMethod =
+                            PasswordTransformationMethod.getInstance()
+                    }
+                    else -> {
+                        etLoginPassword.transformationMethod =
+                            HideReturnsTransformationMethod.getInstance()
+                    }
+                }
+            }
+        }
+    }
+}
