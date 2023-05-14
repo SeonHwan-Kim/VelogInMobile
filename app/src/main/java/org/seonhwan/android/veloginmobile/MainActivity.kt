918624354b54d90ac0ec8fd3154e4e8c6b698258
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-05-15 01:10:43 +0900

    feat : bottomnavigation fragment와 연결 #2

--- app/src/main/java/org/seonhwan/android/veloginmobile/MainActivity.kt
+++ app/src/main/java/org/seonhwan/android/veloginmobile/MainActivity.kt
@@ -2,7 +2,12 @@ package org.seonhwan.android.veloginmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import org.seonhwan.android.veloginmobile.databinding.ActivityMainBinding
+import org.seonhwan.android.veloginmobile.presentation.Notification.NotificationFragment
+import org.seonhwan.android.veloginmobile.presentation.Subscribe.SubscribeFragment
+import org.seonhwan.android.veloginmobile.presentation.home.HomeFragment
+import org.seonhwan.android.veloginmobile.presentation.mypage.MypageFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,6 +16,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        this.initLayout()
+    }
+
+    private fun initLayout(){
         binding.bnvMain.itemIconTintList = null
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fcv_main)
+        if(currentFragment == null){
+            supportFragmentManager.beginTransaction().add(R.id.fcv_main, HomeFragment()).commit()
+        }
+        binding.bnvMain.setOnItemSelectedListener {item ->
+            when (item.itemId) {
+                R.id.menu_home -> this.changeFragment(HomeFragment())
+                R.id.menu_subscribe -> this.changeFragment(SubscribeFragment())
+                R.id.menu_notification -> this.changeFragment(NotificationFragment())
+                else -> this.changeFragment(MypageFragment())
+            }
+            return@setOnItemSelectedListener true
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fcv_main, fragment)
+            .commit()
     }
 }
