636f0986dae0c620468d8ae3b34341398efb01cf
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-05-18 03:21:18 +0900

    add : AddTagActivity 추가 #2

---
+++ app/src/main/java/org/seonhwan/android/veloginmobile/ui/home/AddTagActivity.kt
@@ -0,0 +1,12 @@
+package org.seonhwan.android.veloginmobile.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.seonhwan.android.veloginmobile.R
+
+class AddTagActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_tag)
+    }
+}
