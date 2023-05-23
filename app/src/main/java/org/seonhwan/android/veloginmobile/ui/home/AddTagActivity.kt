767575fa886b4412182cef8b6f5ab96e16c630ed
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-05-23 22:52:28 +0900

    mod : databinding 사용

--- app/src/main/java/org/seonhwan/android/veloginmobile/ui/home/AddTagActivity.kt
+++ app/src/main/java/org/seonhwan/android/veloginmobile/ui/home/AddTagActivity.kt
@@ -1,10 +1,12 @@
 package org.seonhwan.android.veloginmobile.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import org.seonhwan.android.veloginmobile.R
+import org.seonhwan.android.veloginmobile.databinding.ActivityAddTagBinding
+import org.seonhwan.android.veloginmobile.util.binding.BindingActivity
 
-class AddTagActivity : AppCompatActivity() {
+class AddTagActivity : BindingActivity<ActivityAddTagBinding>(R.layout.activity_add_tag) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_tag)
