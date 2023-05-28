fcedb6c051120b21e2f3f7230677cc361e4cbbd9
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-05-28 23:44:31 +0900

    feat : 서버 통신 방식 변경

--- app/src/main/java/org/seonhwan/android/veloginmobile/ui/home/HomeFragment.kt
+++ app/src/main/java/org/seonhwan/android/veloginmobile/ui/home/HomeFragment.kt
@@ -1,24 +1,34 @@
 package org.seonhwan.android.veloginmobile.ui.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.google.android.material.tabs.TabLayout
+import dagger.hilt.android.AndroidEntryPoint
 import org.seonhwan.android.veloginmobile.R
 import org.seonhwan.android.veloginmobile.databinding.FragmentHomeBinding
 import org.seonhwan.android.veloginmobile.presentation.home.VelogAdapter
+import org.seonhwan.android.veloginmobile.util.UiState
 import org.seonhwan.android.veloginmobile.util.binding.BindingFragment
+import timber.log.Timber
 
+@AndroidEntryPoint
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     private val viewModel by viewModels<HomeViewModel>()
+    private var postAdapter: VelogAdapter? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initAdapter()
         startSecondTabItem()
         onClickTabBar()
         viewModel.getTag()
+    }
+
+    private fun initAdapter() {
+        postAdapter = VelogAdapter()
+        binding.rvHomePost.adapter = postAdapter
     }
 
     private fun onClickTabBar() {
@@ -32,6 +42,7 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home
 //                        startSecondTabItem()
                         initPost()
                     }
+
                     else -> {
                         initPost()
                     }
@@ -54,6 +65,21 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home
 
     private fun initPost() {
         viewModel.getTagPost()
+        viewModel.tagPostListState.observe(this) { state ->
+            when (state) {
+                is UiState.Success -> {
+                    postAdapter?.submitList(viewModel.tagPostList.value)
+                }
+
+                is UiState.Failure -> {
+                    Timber.tag("tagPostListState").e("Failure")
+                }
+
+                is UiState.Error -> {
+                    Timber.tag("tagPostListState").e("Error")
+                }
+            }
+        }
         viewModel.tagPostList.observe(this) {
             binding.rvHomePost.adapter = VelogAdapter().apply {
                 submitList(it)
--- app/src/main/java/org/seonhwan/android/veloginmobile/ui/home/HomeViewModel.kt
+++ app/src/main/java/org/seonhwan/android/veloginmobile/ui/home/HomeViewModel.kt
@@ -1,49 +1,57 @@
 package org.seonhwan.android.veloginmobile.ui.home
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import org.seonhwan.android.veloginmobile.data.ServicePool
-import org.seonhwan.android.veloginmobile.data.model.response.ResponsePostDto
-import org.seonhwan.android.veloginmobile.data.model.response.ResponseTagPostDto
+import org.seonhwan.android.veloginmobile.domain.entity.Post
+import org.seonhwan.android.veloginmobile.domain.repository.TagRepository
+import org.seonhwan.android.veloginmobile.util.UiState
+import timber.log.Timber
+import javax.inject.Inject
 
-class HomeViewModel : ViewModel() {
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val tagRepository: TagRepository,
+) : ViewModel() {
     private val _tagList = MutableLiveData<List<String>>()
     val tagList: LiveData<List<String>> get() = _tagList
 
-    private val _tagPostList = MutableLiveData<List<ResponsePostDto>>()
-    val tagPostList: LiveData<List<ResponsePostDto>> get() = _tagPostList
+    private val _tagListState = MutableLiveData<UiState>()
+    val tagListState: LiveData<UiState>
+        get() = _tagListState
+
+    private val _tagPostList = MutableLiveData<List<Post>>()
+    val tagPostList: LiveData<List<Post>> get() = _tagPostList
+
+    private val _tagPostListState = MutableLiveData<UiState>()
+    val tagPostListState: LiveData<UiState>
+        get() = _tagPostListState
 
     fun getTag() {
         viewModelScope.launch {
-            getTagList().onSuccess {
-                _tagList.value = it
-            }.onFailure {
-                Log.d("failure", it.toString())
+            tagRepository.GetTag().onSuccess { response ->
+                _tagListState.value = UiState.Success
+                _tagList.value = response
+            }.onFailure { throwable ->
+                _tagListState.value = UiState.Failure(null)
+                Timber.tag("onFailure").e(throwable.toString())
             }
         }
-    }
-
-    private suspend fun getTagList(): Result<List<String>> = kotlin.runCatching {
-        ServicePool.getTagService.getTag()
     }
 
     fun getTagPost() {
         viewModelScope.launch {
-            getTagPostList().onSuccess {
-                _tagPostList.value = it.tagPostDtoList
-                Log.d("success Tag Post List", it.tagPostDtoList.toString())
-            }.onFailure {
-                Log.d("failure", it.toString())
+            tagRepository.GetTagPost().onSuccess { response ->
+                _tagPostListState.value = UiState.Success
+                _tagPostList.value = response
+                Timber.tag("success Tag Post List").d(response.toString())
+            }.onFailure { throwable ->
+                _tagPostListState.value = UiState.Failure(null)
+                Timber.tag("failure Tag Post List").e(throwable)
             }
         }
     }
-
-    private suspend fun getTagPostList(): Result<ResponseTagPostDto<ResponsePostDto>> =
-        kotlin.runCatching {
-            ServicePool.getTagPostService.getTagPost()
-        }
 }
