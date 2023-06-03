303c3cf454a015487abb4a08c20974eac016b805
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-06-04 01:58:39 +0900

    [DELETE/#6] AddTagUiState 삭제

--- app/src/main/java/org/seonhwan/android/veloginmobile/ui/addtag/AddTagUiState.kt
+++
@@ -1,8 +0,0 @@
-package org.seonhwan.android.veloginmobile.ui.addtag
-
-sealed class AddTagUiState {
-    object Success : AddTagUiState()
-    object Empty : AddTagUiState()
-    data class Failure(val code: Int?) : AddTagUiState()
-    object Error : AddTagUiState()
-}
