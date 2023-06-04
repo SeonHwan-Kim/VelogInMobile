4e38452ca8184086bfd4fbcd776c3b8c2164aa1c
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-06-05 04:57:46 +0900

    [ADD/#8] webViewViewModel 추가

---
+++ app/src/main/java/org/seonhwan/android/veloginmobile/ui/webview/WebViewViewModel.kt
@@ -0,0 +1,44 @@
+package org.seonhwan.android.veloginmobile.ui.webview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import org.seonhwan.android.veloginmobile.domain.repository.SubscribeRepository
+import org.seonhwan.android.veloginmobile.ui.addtag.AddTagViewModel.Companion.CODE_400
+import org.seonhwan.android.veloginmobile.util.UiState
+import org.seonhwan.android.veloginmobile.util.UiState.Error
+import org.seonhwan.android.veloginmobile.util.UiState.Failure
+import org.seonhwan.android.veloginmobile.util.UiState.Success
+import retrofit2.HttpException
+import javax.inject.Inject
+
+class WebViewViewModel @Inject constructor(
+    private val subscribeRepository: SubscribeRepository,
+) : ViewModel() {
+    private val _addSubscriberState = MutableLiveData<UiState>()
+    val addSubscriberState: LiveData<UiState>
+        get() = _addSubscriberState
+
+    fun addSubscriber(name: String) {
+        viewModelScope.launch {
+            subscribeRepository.addSubscriber(name)
+                .onSuccess {
+                    _addSubscriberState.value = Success
+                }
+                .onFailure { throwable ->
+                    if (throwable is HttpException) {
+                        when (throwable.code()) {
+                            CODE_400 -> {
+                                Failure(CODE_400)
+                            }
+                            else -> {
+                                Error
+                            }
+                        }
+                    }
+                }
+        }
+    }
+}
