a68d3181ebb5f560a6f4d1f0051cb143f8a2ed1e
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-06-08 21:04:08 +0900

    [MOD/#8] subscribe 수정

--- app/src/main/java/org/seonhwan/android/veloginmobile/data/repository/SubscribeRepositoryImpl.kt
+++ app/src/main/java/org/seonhwan/android/veloginmobile/data/repository/SubscribeRepositoryImpl.kt
@@ -1,7 +1,6 @@
 package org.seonhwan.android.veloginmobile.data.repository
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import org.seonhwan.android.veloginmobile.data.source.SubscribeSource
 import org.seonhwan.android.veloginmobile.domain.entity.Post
@@ -12,20 +11,14 @@ class SubscribeRepositoryImpl @Inject constructor(
     private val subscribeSource: SubscribeSource,
 ) : SubscribeRepository {
     override suspend fun addSubscriber(name: String): Flow<Unit> = flow {
-        subscribeSource.addSubscriber(name).collect {
-            emit(it)
-        }
+        emit(subscribeSource.addSubscriber(name))
     }
 
     override suspend fun deleteSubscriber(name: String): Flow<Unit> = flow {
-        subscribeSource.deleteSubscriber(name).collect {
-            emit(it)
-        }
+        emit(subscribeSource.deleteSubscriber(name))
     }
 
     override suspend fun getSubscriberPost(): Flow<List<Post>> = flow {
-        subscribeSource.getSubscriberPost().collect { response ->
-            emit(response.subscribePostDtoList.map { data -> data.toPostEntity() })
-        }
+        emit(subscribeSource.getSubscriberPost().subscribePostDtoList.map { data -> data.toPostEntity() })
     }
 }
