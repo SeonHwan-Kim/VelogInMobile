fc1371960913da43e93d4eb0f34573639a98d7f8
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-06-08 21:04:22 +0900

    [MOD/#8] tag emit

--- app/src/main/java/org/seonhwan/android/veloginmobile/data/repository/TagRepositoryImpl.kt
+++ app/src/main/java/org/seonhwan/android/veloginmobile/data/repository/TagRepositoryImpl.kt
@@ -10,28 +10,23 @@ import javax.inject.Inject
 class TagRepositoryImpl @Inject constructor(
     private val tagSource: TagSource,
 ) : TagRepository {
-    override suspend fun getTag(): Flow<List<String>> =
-        flow {
-            tagSource.getTagList()
-        }
+    override suspend fun getTag(): Flow<List<String>> = flow {
+        emit(tagSource.getTagList())
+    }
 
-    override suspend fun getTagPost(): Flow<List<Post>> =
-        flow {
-            tagSource.getTagPostList()
-        }
+    override suspend fun getTagPost(): Flow<List<Post>> = flow {
+        emit(tagSource.getTagPostList().tagPostDtoList.map { data -> data.toPostEntity() })
+    }
 
-    override suspend fun postAddTag(tag: String): Flow<Unit> =
-        flow {
-            tagSource.postAddTag(tag)
-        }
+    override suspend fun postAddTag(tag: String): Flow<Unit> = flow {
+        emit(tagSource.postAddTag(tag))
+    }
 
-    override suspend fun deleteTag(tag: String): Flow<Unit> =
-        flow {
-            tagSource.deleteTag(tag)
-        }
+    override suspend fun deleteTag(tag: String): Flow<Unit> = flow {
+        emit(tagSource.deleteTag(tag))
+    }
 
-    override suspend fun getPopularTag(): Flow<List<String>> =
-        flow {
-            tagSource.getPopularTag()
-        }
+    override suspend fun getPopularTag(): Flow<List<String>> = flow {
+        emit(tagSource.getPopularTag())
+    }
 }
