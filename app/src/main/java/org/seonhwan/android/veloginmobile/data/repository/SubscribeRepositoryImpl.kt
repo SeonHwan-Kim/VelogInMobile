33288fd10d7d0c368bf003868ae0f968879752ca
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-06-08 20:01:26 +0900

    [MOD/#8] Subscribe flow 변경

--- app/src/main/java/org/seonhwan/android/veloginmobile/data/repository/SubscribeRepositoryImpl.kt
+++ app/src/main/java/org/seonhwan/android/veloginmobile/data/repository/SubscribeRepositoryImpl.kt
@@ -1,5 +1,8 @@
 package org.seonhwan.android.veloginmobile.data.repository
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 import org.seonhwan.android.veloginmobile.data.source.SubscribeSource
 import org.seonhwan.android.veloginmobile.domain.entity.Post
 import org.seonhwan.android.veloginmobile.domain.repository.SubscribeRepository
@@ -8,20 +11,21 @@ import javax.inject.Inject
 class SubscribeRepositoryImpl @Inject constructor(
     private val subscribeSource: SubscribeSource,
 ) : SubscribeRepository {
-    override suspend fun addSubscriber(name: String): Result<Unit> =
-        runCatching {
-            subscribeSource.addSubscriber(name)
+    override suspend fun addSubscriber(name: String): Flow<Unit> = flow {
+        subscribeSource.addSubscriber(name).collect {
+            emit(it)
         }
+    }
 
-    override suspend fun deleteSubscriber(name: String): Result<Unit> =
-        runCatching {
-            subscribeSource.deleteSubscriber(name)
+    override suspend fun deleteSubscriber(name: String): Flow<Unit> = flow {
+        subscribeSource.deleteSubscriber(name).collect {
+            emit(it)
         }
+    }
 
-    override suspend fun getSubscriberPost(): Result<List<Post>> =
-        runCatching {
-            subscribeSource.getSubscriberPost().subscribePostDtoList.map { data ->
-                data.toPostEntity()
-            }
+    override suspend fun getSubscriberPost(): Flow<List<Post>> = flow {
+        subscribeSource.getSubscriberPost().collect { response ->
+            emit(response.subscribePostDtoList.map { data -> data.toPostEntity() })
         }
+    }
 }
--- app/src/main/java/org/seonhwan/android/veloginmobile/data/source/SubscribeSource.kt
+++ app/src/main/java/org/seonhwan/android/veloginmobile/data/source/SubscribeSource.kt
@@ -1,17 +1,24 @@
 package org.seonhwan.android.veloginmobile.data.source
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import org.seonhwan.android.veloginmobile.data.model.response.ResponsePostDto
+import org.seonhwan.android.veloginmobile.data.model.response.ResponseSubscriberPostDto
 import org.seonhwan.android.veloginmobile.data.service.SubscribeService
 import javax.inject.Inject
 
 class SubscribeSource @Inject constructor(
     private val subscribeService: SubscribeService,
 ) {
-    suspend fun addSubscriber(name: String) =
-        subscribeService.addSubscriber(name, "0")
+    suspend fun addSubscriber(name: String): Flow<Unit> = flow {
+        emit(subscribeService.addSubscriber(name, "0"))
+    }
 
-    suspend fun deleteSubscriber(name: String) =
-        subscribeService.deleteSubscriber(name)
+    suspend fun deleteSubscriber(name: String): Flow<Unit> = flow {
+        emit(subscribeService.deleteSubscriber(name))
+    }
 
-    suspend fun getSubscriberPost() =
-        subscribeService.getSubscriberPost()
+    suspend fun getSubscriberPost(): Flow<ResponseSubscriberPostDto<ResponsePostDto>> = flow {
+        emit(subscribeService.getSubscriberPost())
+    }
 }
