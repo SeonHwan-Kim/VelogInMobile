2a9b9e38afc361bde38379ab50c83314c9e16b4f
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-06-09 02:07:59 +0900

    [MOD/#8] 불필요한 주석 제거

--- app/src/main/java/org/seonhwan/android/veloginmobile/domain/repository/TagRepository.kt
+++ app/src/main/java/org/seonhwan/android/veloginmobile/domain/repository/TagRepository.kt
@@ -6,15 +6,11 @@ import org.seonhwan.android.veloginmobile.domain.entity.Post
 interface TagRepository {
     suspend fun getTag(): Flow<List<String>>
 
-//    suspend fun getTagPost(): Result<List<Post>>
     suspend fun getTagPost(): Flow<List<Post>>
 
-//    suspend fun postAddTag(tag: String): Result<Unit>
     suspend fun postAddTag(tag: String): Flow<Unit>
 
-//    suspend fun deleteTag(tag: String): Result<Unit>
     suspend fun deleteTag(tag: String): Flow<Unit>
 
-//    suspend fun getPopularTag(): Result<List<String>>
     suspend fun getPopularTag(): Flow<List<String>>
 }
