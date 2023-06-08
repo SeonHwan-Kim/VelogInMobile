08a3c3a60787f66c05e9d6b1591ca8926dede792
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-06-08 20:02:33 +0900

    [MOD/#8] Flow구현, Loading 구현

--- app/src/main/java/org/seonhwan/android/veloginmobile/ui/home/HomeFragment.kt
+++ app/src/main/java/org/seonhwan/android/veloginmobile/ui/home/HomeFragment.kt
@@ -8,16 +8,22 @@ import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.tabs.TabLayout
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import org.seonhwan.android.veloginmobile.R
 import org.seonhwan.android.veloginmobile.databinding.FragmentHomeBinding
 import org.seonhwan.android.veloginmobile.ui.addtag.AddTagActivity
 import org.seonhwan.android.veloginmobile.ui.home.HomeViewModel.Companion.CODE_202
+import org.seonhwan.android.veloginmobile.ui.home.HomeViewModel.Companion.NETWORK_ERR
 import org.seonhwan.android.veloginmobile.ui.home.VelogAdapter.Companion.VELOG
 import org.seonhwan.android.veloginmobile.ui.webview.WebViewActivity
-import org.seonhwan.android.veloginmobile.util.UiState.Error
 import org.seonhwan.android.veloginmobile.util.UiState.Failure
+import org.seonhwan.android.veloginmobile.util.UiState.Loading
 import org.seonhwan.android.veloginmobile.util.UiState.Success
 import org.seonhwan.android.veloginmobile.util.binding.BindingFragment
 import org.seonhwan.android.veloginmobile.util.extension.showToast
@@ -39,28 +45,31 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home
     }
 
     private fun initTabItem() {
-        viewModel.tagListState.observe(viewLifecycleOwner) { state ->
-            when (state) {
-                is Success -> {
-                    with(binding) {
-                        addToolbarTag("", R.drawable.ic_plus, 0, false)
-                        addToolbarTag("트렌드", null, 1, true)
-                        addToolbarTag("팔로우", null, 2, false)
-                        viewModel.tagList.value?.mapIndexed { index, tag ->
-                            addToolbarTag(tag, null, index + 3, false)
+        viewModel.tagList
+            .flowWithLifecycle(lifecycle)
+            .onEach { event ->
+                when (event) {
+                    is Loading -> {}
+                    is Success -> {
+                        with(binding) {
+                            addToolbarTag("", R.drawable.ic_plus, 0, false)
+                            addToolbarTag("트렌드", null, 1, true)
+                            addToolbarTag("팔로우", null, 2, false)
+                            event.data.mapIndexed { index, tag ->
+                                addToolbarTag(tag, null, index + 3, false)
+                            }
                         }
                     }
-                }
 
-                is Failure -> {
-                    Timber.tag("tagListState").e("Failure")
+                    is Failure -> {
+                        when (event.code) {
+                            NETWORK_ERR -> requireActivity().showToast("네트워크 상태를 확인해주세요")
+                            else -> requireActivity().showToast("문제가 발생하였습니다")
+                        }
+                        Timber.tag("tagListState").e("Failure")
+                    }
                 }
-
-                is Error -> {
-                    Timber.tag("tagListState").e("Error")
-                }
-            }
-        }
+            }.launchIn(lifecycleScope)
     }
 
     private fun addToolbarTag(tag: String, icon: Int?, position: Int, setSelected: Boolean) {
@@ -142,34 +151,42 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home
         ActivityResultContracts.StartActivityForResult(),
     ) { result: ActivityResult ->
         if (result.resultCode == RESULT_OK) {
-            viewModel.getTagPost()
+            when (binding.tabHomeTabbar.selectedTabPosition) {
+                1 -> viewModel.getTagPost()
+                2 -> viewModel.getSubscriberPost()
+                else -> viewModel.getTagPost()
+            }
         }
     }
 
     private fun initPost() {
-        viewModel.postListState.observe(viewLifecycleOwner) { state ->
-            when (state) {
-                is Success -> {
-                    postAdapter?.submitList(viewModel.postList.value)
-                }
+        viewModel.postList
+            .flowWithLifecycle(lifecycle)
+            .onEach { event ->
+                when (event) {
+                    is Loading -> binding.pbHomeLoading.visibility = View.VISIBLE
 
-                is Failure -> {
-                    when (state.code) {
-                        CODE_202 -> {
-                            requireActivity().showToast("구독한 사람이 없습니다")
-                        }
+                    is Success -> {
+                        binding.pbHomeLoading.visibility = View.GONE
+                        postAdapter?.submitList(event.data)
+                    }
 
-                        else -> {
-                            requireActivity().showToast("Failure")
+                    is Failure -> {
+                        binding.pbHomeLoading.visibility = View.VISIBLE
+                        when (event.code) {
+                            CODE_202 -> {
+                                binding.pbHomeLoading.visibility = View.GONE
+                                requireActivity().showToast("구독자가 없습니다")
+                            }
+
+                            else -> {
+                                binding.pbHomeLoading.visibility = View.GONE
+                                requireActivity().showToast("문제가 발생하였습니다")
+                            }
                         }
                     }
                 }
-
-                is Error -> {
-                    requireActivity().showToast("문제가 발생하였습니다")
-                }
-            }
-        }
+            }.launchIn(lifecycleScope)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/org/seonhwan/android/veloginmobile/ui/home/HomeViewModel.kt
+++ app/src/main/java/org/seonhwan/android/veloginmobile/ui/home/HomeViewModel.kt
@@ -1,20 +1,24 @@
 package org.seonhwan.android.veloginmobile.ui.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import org.seonhwan.android.veloginmobile.domain.entity.Post
 import org.seonhwan.android.veloginmobile.domain.repository.SubscribeRepository
 import org.seonhwan.android.veloginmobile.domain.repository.TagRepository
 import org.seonhwan.android.veloginmobile.util.UiState
-import org.seonhwan.android.veloginmobile.util.UiState.Error
 import org.seonhwan.android.veloginmobile.util.UiState.Failure
+import org.seonhwan.android.veloginmobile.util.UiState.Loading
 import org.seonhwan.android.veloginmobile.util.UiState.Success
 import retrofit2.HttpException
 import timber.log.Timber
+import java.net.ConnectException
 import javax.inject.Inject
 
 @HiltViewModel
@@ -22,21 +26,13 @@ class HomeViewModel @Inject constructor(
     private val tagRepository: TagRepository,
     private val subscribeRepository: SubscribeRepository,
 ) : ViewModel() {
-    private val _tagList = MutableLiveData<List<String>>()
-    val tagList: LiveData<List<String>>
+    private val _tagList = MutableSharedFlow<UiState<List<String>>>()
+    val tagList: SharedFlow<UiState<List<String>>>
         get() = _tagList
 
-    private val _tagListState = MutableLiveData<UiState>()
-    val tagListState: LiveData<UiState>
-        get() = _tagListState
-
-    private val _postList = MutableLiveData<List<Post>>()
-    val postList: LiveData<List<Post>>
+    private val _postList = MutableSharedFlow<UiState<List<Post>>>()
+    val postList: SharedFlow<UiState<List<Post>>>
         get() = _postList
-
-    private val _postListState = MutableLiveData<UiState>()
-    val postListState: LiveData<UiState>
-        get() = _postListState
 
     init {
         getTag()
@@ -44,52 +40,63 @@ class HomeViewModel @Inject constructor(
 
     fun getTag() {
         viewModelScope.launch {
-            tagRepository.getTag().onSuccess { response ->
-                _tagList.value = response
-                Timber.tag("getTage").d(response.toString())
-                _tagListState.value = Success
-            }.onFailure { throwable ->
-                Timber.tag("onFailure").e(throwable.toString())
-                _tagListState.value = Failure(null)
-            }
+            tagRepository.getTag()
+                .catch { error ->
+                    when (error) {
+                        is ConnectException -> {
+                            _tagList.emit(Failure(NETWORK_ERR))
+                        }
+
+                        else -> {
+                            _tagList.emit(Failure(null))
+                        }
+                    }
+                }
+                .collect { response ->
+                    _tagList.emit(Success(response))
+                }
         }
     }
 
     fun getTagPost() {
         viewModelScope.launch {
-            tagRepository.getTagPost().onSuccess { response ->
-                _postList.value = response
-                _postListState.value = Success
-            }.onFailure { throwable ->
-                _postListState.value = Failure(null)
-            }
+            tagRepository.getTagPost()
+                .onStart { _postList.emit(Loading) }
+                .catch { error ->
+                    when (error) {
+                        is ConnectException -> _postList.emit(Failure(NETWORK_ERR))
+
+                        else -> _postList.emit(Failure(null))
+                    }
+                }
+                .collect { response ->
+                    _postList.emit(Success(response))
+                }
         }
     }
 
     fun getSubscriberPost() {
         viewModelScope.launch {
             subscribeRepository.getSubscriberPost()
-                .onSuccess { response ->
-                    _postList.value = response
-                    _postListState.value = Success
-                }
-                .onFailure { throwable ->
+                .onStart { _postList.emit(Loading) }
+                .catch { throwable ->
                     if (throwable is HttpException) {
-                        when (throwable.code()) {
-                            CODE_202 -> {
-                                _postListState.value = Failure(CODE_202)
-                            }
-
-                            else -> {
-                                _postListState.value = Error
-                            }
-                        }
+                        _postList.emit(Failure(throwable.code()))
+                        Timber.d(throwable.message())
+                    } else {
+                        _postList.emit(Failure(null))
+                        Timber.d(throwable)
                     }
+                }
+                .collect { response ->
+                    _postList.emit(Success<List<Post>>(response))
+                    Timber.d(response.toString())
                 }
         }
     }
 
     companion object {
         const val CODE_202 = 202
+        const val NETWORK_ERR = 0
     }
 }
