41249b008fd4e0088d1da7273836d5b510666666
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-06-11 20:44:38 +0900

    [ADD/#10] notificatrion to scrap

--- app/src/main/java/org/seonhwan/android/veloginmobile/VelogApplication.kt
+++ app/src/main/java/org/seonhwan/android/veloginmobile/VelogApplication.kt
@@ -1,13 +1,39 @@
 package org.seonhwan.android.veloginmobile
 
 import android.app.Application
+import androidx.room.Room
 import dagger.hilt.android.HiltAndroidApp
+import org.seonhwan.android.veloginmobile.data.local.database.ScrapPostDatabase
+import org.seonhwan.android.veloginmobile.util.UserSharedPreferences
 import timber.log.Timber
 
 @HiltAndroidApp
-class VelogApplication : Application(){
+class VelogApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        appInstance = this
+
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+
+        userPrefs = UserSharedPreferences(this)
+
+        appDatabaseInstance = Room.databaseBuilder(
+            appInstance.applicationContext,
+            ScrapPostDatabase::class.java,
+            "scrapDatabase.db",
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    companion object {
+        lateinit var userPrefs: UserSharedPreferences
+            private set
+
+        lateinit var appInstance: VelogApplication
+            private set
+
+        lateinit var appDatabaseInstance: ScrapPostDatabase
+            private set
     }
 }
