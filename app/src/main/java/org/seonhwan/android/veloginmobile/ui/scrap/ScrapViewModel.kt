9755f51958cf4e1ce526f131a40b2d0a4e2af6a1
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-06-12 22:43:07 +0900

    [ADD/#10] ScrapViewModel 추가, 구현

---
+++ app/src/main/java/org/seonhwan/android/veloginmobile/ui/scrap/ScrapViewModel.kt
@@ -0,0 +1,62 @@
+package org.seonhwan.android.veloginmobile.ui.scrap
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+import org.seonhwan.android.veloginmobile.data.local.model.ScrapPost
+import org.seonhwan.android.veloginmobile.domain.entity.Post
+import org.seonhwan.android.veloginmobile.domain.entity.toScrapPost
+import org.seonhwan.android.veloginmobile.domain.repository.local.ScrapPostRepository
+import org.seonhwan.android.veloginmobile.util.UiState
+import org.seonhwan.android.veloginmobile.util.UiState.Failure
+import org.seonhwan.android.veloginmobile.util.UiState.Loading
+import org.seonhwan.android.veloginmobile.util.UiState.Success
+import retrofit2.HttpException
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class ScrapViewModel @Inject constructor(
+    private val scrapPostRepository: ScrapPostRepository,
+) : ViewModel() {
+    private val _getAllScrapPostState = MutableSharedFlow<UiState<List<ScrapPost>>>()
+    val getAllScrapPostState: SharedFlow<UiState<List<ScrapPost>>>
+        get() = _getAllScrapPostState
+
+    init {
+        getAllScrapPost()
+    }
+
+    fun getAllScrapPost() {
+        viewModelScope.launch {
+            scrapPostRepository.getAllScrapPost()
+                .onStart { _getAllScrapPostState.emit(Loading) }
+                .catch { error ->
+                    if (error is HttpException) {
+                        _getAllScrapPostState.emit(Failure(error.code()))
+                    }
+                }
+                .collect { response ->
+                    Timber.d(response.toString())
+                    _getAllScrapPostState.emit(Success(response))
+                }
+        }
+    }
+
+    fun scrapPost(post: Post, folder: String?) {
+        viewModelScope.launch {
+            scrapPostRepository.addScrapPost(post.toScrapPost(folder))
+        }
+    }
+
+    fun deleteScrapPost(url: String) {
+        viewModelScope.launch {
+            scrapPostRepository.deleteScrapPost(url)
+        }
+    }
+}
