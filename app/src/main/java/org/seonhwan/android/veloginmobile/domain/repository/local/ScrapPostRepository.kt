92dbd6e0f979872b140a70afefbeebe79075e86b
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-06-15 02:00:36 +0900

    [ADD/#10] scrapPostRepository 추가

---
+++ app/src/main/java/org/seonhwan/android/veloginmobile/domain/repository/local/ScrapPostRepository.kt
@@ -0,0 +1,12 @@
+package org.seonhwan.android.veloginmobile.domain.repository.local
+
+import kotlinx.coroutines.flow.Flow
+import org.seonhwan.android.veloginmobile.data.local.model.ScrapPost
+
+interface ScrapPostRepository {
+    suspend fun addScrapPost(post: ScrapPost)
+
+    suspend fun getAllScrapPost(): Flow<List<ScrapPost>>
+
+    suspend fun deleteScrapPost(url: String)
+}
