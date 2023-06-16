63665ca8762fd8ef85e60eb26b08226591ba8ba9
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-06-16 23:34:58 +0900

    [FEAT/#10] scrap 구현

--- app/src/main/java/org/seonhwan/android/veloginmobile/ui/home/VelogAdapter.kt
+++ app/src/main/java/org/seonhwan/android/veloginmobile/ui/home/VelogAdapter.kt
@@ -1,5 +1,6 @@
 package org.seonhwan.android.veloginmobile.ui.home
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
@@ -12,14 +13,26 @@ import org.seonhwan.android.veloginmobile.util.extension.BookmarkSnackbar
 
 class VelogAdapter(
     private val intentToWebView: (Post) -> Unit,
+    private val scrapPost: (Post) -> Unit,
+    private val deleteScrapPost: (String) -> Unit,
+    private val scrapPostList: List<Post>?,
 ) : ListAdapter<Post, VelogAdapter.VelogViewHolder>(diffUtil) {
 
     class VelogViewHolder(private val binding: ItemVelogBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun onBind(post: Post) {
+        fun onBind(
+            post: Post,
+            scrapPost: (Post) -> Unit,
+            deleteScrapPost: (String) -> Unit,
+            scrapPostList: List<Post>?,
+        ) {
             with(binding) {
                 data = post
                 ivVelogImg.load(post.img)
+                if (scrapPostList != null) {
+                    if (scrapPostList.contains(post)) ibVelogBookmark.isSelected = true
+                }
+                Log.d("velogViewHolder", scrapPostList.toString())
                 rvVelogTag.adapter = VelogTagAdapter().apply {
                     if (post.tag.size > 3) {
                         submitList(post.tag.slice(0..2))
@@ -27,9 +40,27 @@ class VelogAdapter(
                         submitList(post.tag)
                     }
                 }
+                onClickBookmark(post, scrapPost, deleteScrapPost)
+            }
+        }
+
+        private fun onClickBookmark(
+            post: Post,
+            scrapPost: (Post) -> Unit,
+            deleteScrapPost: (String) -> Unit,
+        ) {
+            with(binding) {
                 ibVelogBookmark.setOnClickListener {
-                    ibVelogBookmark.isSelected = !ibVelogBookmark.isSelected
-                    BookmarkSnackbar.make(binding.root, "스크랩 했습니다.").show()
+                    with(ibVelogBookmark) {
+                        if (isSelected) {
+                            deleteScrapPost(post.url)
+                            BookmarkSnackbar.make(binding.root, "스크랩을 취소하였습니다.").show()
+                        } else {
+                            scrapPost(post)
+                            BookmarkSnackbar.make(binding.root, "스크랩 했습니다.").show()
+                        }
+                        isSelected = !isSelected
+                    }
                 }
             }
         }
@@ -50,12 +81,12 @@ class VelogAdapter(
         holder.itemView.setOnClickListener {
             intentToWebView(post)
         }
-        holder.onBind(post)
+        holder.onBind(post, scrapPost, deleteScrapPost, scrapPostList)
     }
 
     companion object {
         private val diffUtil = DiffCallback<Post>(
-            onItemsTheSame = { old, new -> old.summary == new.summary },
+            onItemsTheSame = { old, new -> old.url == new.url },
             onContentsTheSame = { old, new -> old == new },
         )
 
