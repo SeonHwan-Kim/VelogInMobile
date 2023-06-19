886621c2f47e73360c0792faee4834d68259e999
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-06-20 02:37:08 +0900

    [ADD/#12] Database folder 추가

--- app/src/main/java/org/seonhwan/android/veloginmobile/data/local/database/ScrapPostDatabase.kt
+++ app/src/main/java/org/seonhwan/android/veloginmobile/data/local/database/ScrapPostDatabase.kt
@@ -3,11 +3,15 @@ package org.seonhwan.android.veloginmobile.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import org.seonhwan.android.veloginmobile.data.local.dao.FolderDao
 import org.seonhwan.android.veloginmobile.data.local.dao.ScrapPostDao
+import org.seonhwan.android.veloginmobile.data.local.model.Folder
 import org.seonhwan.android.veloginmobile.data.local.model.ScrapPost
 
-@Database(entities = [ScrapPost::class], version = 4)
+@Database(entities = [ScrapPost::class, Folder::class], version = 5)
 @TypeConverters(ScrapPostConverter::class)
 abstract class ScrapPostDatabase : RoomDatabase() {
     abstract fun scrapPostDao(): ScrapPostDao
+
+    abstract fun folderDao(): FolderDao
 }
