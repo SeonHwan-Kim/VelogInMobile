3ca68ae8e4a29b846da7d0b0ca903114ea8692b9
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-06-26 15:32:52 +0900

    [ADD/#12] 폴더 추가 dialog 추가

---
+++ app/src/main/java/org/seonhwan/android/veloginmobile/ui/scrap/AddFolderDialog.kt
@@ -0,0 +1,14 @@
+package org.seonhwan.android.veloginmobile.ui.scrap
+
+import android.os.Bundle
+import android.view.View
+import org.seonhwan.android.veloginmobile.R
+import org.seonhwan.android.veloginmobile.databinding.FragmentAddFolderDialogBinding
+import org.seonhwan.android.veloginmobile.util.binding.BindingDialog
+
+class AddFolderDialog() :
+    BindingDialog<FragmentAddFolderDialogBinding>(R.layout.fragment_add_folder_dialog) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
