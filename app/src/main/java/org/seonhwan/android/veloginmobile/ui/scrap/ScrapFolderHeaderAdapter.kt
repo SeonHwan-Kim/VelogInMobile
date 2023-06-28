ceb2ff06cfb5387c4f19764175c3f1c6cd7330ce
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-06-28 18:38:58 +0900

    [MOD/#12] 폴더 이름 변경 제거

--- app/src/main/java/org/seonhwan/android/veloginmobile/ui/scrap/ScrapFolderHeaderAdapter.kt
+++ app/src/main/java/org/seonhwan/android/veloginmobile/ui/scrap/ScrapFolderHeaderAdapter.kt
@@ -8,17 +8,12 @@ import org.seonhwan.android.veloginmobile.databinding.ItemScrapPostHeaderBinding
 
 class ScrapFolderHeaderAdapter(
     private val onClickDeleteButton: () -> Unit,
-    private val onClickChangeFolderNameButton: () -> Unit,
 ) : RecyclerView.Adapter<ScrapFolderHeaderAdapter.ScrapFolderHeaderViewHolder>() {
     class ScrapFolderHeaderViewHolder(private val binding: ItemScrapPostHeaderBinding) :
         ViewHolder(binding.root) {
         fun onBind(
-            onClickChangeFolderNameButton: () -> Unit,
             onClickDeleteButton: () -> Unit,
         ) {
-            binding.tvScrapPostChangeFolderName.setOnClickListener {
-                onClickChangeFolderNameButton()
-            }
             binding.tvScrapPostDeleteFolder.setOnClickListener {
                 onClickDeleteButton()
             }
@@ -38,6 +33,6 @@ class ScrapFolderHeaderAdapter(
     override fun getItemCount() = 1
 
     override fun onBindViewHolder(holder: ScrapFolderHeaderViewHolder, position: Int) {
-        holder.onBind(onClickChangeFolderNameButton, onClickDeleteButton)
+        holder.onBind(onClickDeleteButton)
     }
 }
