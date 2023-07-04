31a7eeb78dd52a2b927bd065d905245d042cfedd
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-07-05 06:16:31 +0900

    [FEAT/#16] getTagPost 수정

--- app/src/main/java/org/seonhwan/android/veloginmobile/data/remote/service/TagService.kt
+++ app/src/main/java/org/seonhwan/android/veloginmobile/data/remote/service/TagService.kt
@@ -1,18 +1,20 @@
 package org.seonhwan.android.veloginmobile.data.remote.service
 
 import org.seonhwan.android.veloginmobile.data.remote.model.response.ResponsePostDto
-import org.seonhwan.android.veloginmobile.data.remote.model.response.ResponseTagPostDto
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface TagService {
     @GET("tag/gettag")
     suspend fun getTag(): List<String>
 
-    @GET("tag/tagpost")
-    suspend fun getTagPost(): ResponseTagPostDto<ResponsePostDto>
+    @GET("tag/gettagpost")
+    suspend fun getTagPost(
+        @Query("tag") tag: String,
+    ): List<ResponsePostDto>
 
     @POST("tag/addtag")
     suspend fun postAddTag(
